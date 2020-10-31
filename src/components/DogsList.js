7f085076866a3232e0131d8fa1134c7feba842b8
seabelis <seabelis74@gmail.com>
2020-10-31 15:30:55 +0100

    add link for breed

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -1,10 +1,14 @@
 // components/DogsList.js
 
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
 
 export default class DogsList extends Component {
   renderDogBreed(breed) {
-    return <li key={breed}>{breed}</li>
+    return <li key={breed}>
+      <Link to={ '/dog-breeds/${breed}' }>{breed}</Link>
+      </li>
   }
 
   render () {
