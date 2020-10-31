f05161be2a31f3863f2debf08f0b706aeca476b4
seabelis <seabelis74@gmail.com>
2020-10-31 15:03:13 +0100

    add / route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import DogsListContainer from './components/DogsListContainer';
+import { Route } from "react-router-dom"
 
 function App() {
   return (
     <main>
-      <DogsListContainer />
+      <Route exact path="/" component={ DogsListContainer} />
     </main>
   );
 }
