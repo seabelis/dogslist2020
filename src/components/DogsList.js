cf3f782e21e7f7e9aa89b036396b533b157a7a59
seabelis <seabelis74@gmail.com>
2020-10-31 12:40:04 +0100

    loading if null

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -3,11 +3,16 @@
 import React, { Component } from 'react';
 
 export default class DogsList extends Component {
+  state = {
+    dogBreeds: null
+  };
+
   render () {
     return (
       <div className="dogs-list">
         <h1>Dogs List</h1>
+        { this.state.dogBreeds === null && "Loading..."}
       </div>
-    )
+    );
   }
 }
