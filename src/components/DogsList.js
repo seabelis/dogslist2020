a7f65e2c6504c768a51afcc4b27c4b562de29873
seabelis <seabelis74@gmail.com>
2020-10-31 15:38:10 +0100

    fixed path

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
 
 export default class DogsList extends Component {
   renderDogBreed(breed) {
-    return <li key={breed}>
-      <Link to={ '/dog-breeds/${breed}' }>{breed}</Link>
+    return <li key={breed}> 
+      <Link to={ `/dog-breeds/${breed}` }>{breed}</Link>
       </li>
   }
 
