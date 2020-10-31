6cda8c011f318975afec9987fc7cd9eca6de7701
seabelis <seabelis74@gmail.com>
2020-10-31 15:10:32 +0100

    add DogBreedImages component and route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import DogsListContainer from './components/DogsListContainer';
 import { Route } from "react-router-dom"
+import DogBreedImages from './components/DogBreedImages';
 
 function App() {
   return (
     <main>
       <Route exact path="/" component={ DogsListContainer} />
+      <Route path="/dog-breeds/:breed" component={DogBreedImages} />
     </main>
   );
 }
