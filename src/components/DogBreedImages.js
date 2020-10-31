91b4f7279703b6558a087a1d944dfa4304560047
seabelis <seabelis74@gmail.com>
2020-10-31 16:32:12 +0100

    show some dog images

--- src/components/DogBreedImages.js
+++ src/components/DogBreedImages.js
@@ -1,21 +1,20 @@
-// components/DogBreedImages.js
+// DogBreedImages.js 
 
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function DogBreedImages (props) {
-  const {images, breed} = props;
+	const { images, breed } = props
   
   return (
     <div className="dog-breed-images">
-      <h1>Dog Breed Images</h1>
-      This page will show images of the { breed } breed
-      <div>
-        {images && images.map(url => <img src={ url } alt="dog" />) }
-        {!images && "Loading..." }
-      </div>
+      <h1>Dogs Breed Images</h1>
+		This page will show images of the { breed } breed
+		<div>
+    	  { images && images.map(url => <img src={ url } alt="Dog" />) }
+  	      { !images && 'Loading...' }
+	    </div>
       <Link to="/">Go back to the index</Link>
     </div>
-
   )
 }
--- src/components/DogBreedImagesContainer.js
+++ src/components/DogBreedImagesContainer.js
@@ -11,9 +11,9 @@ export default class DogBreedImagesContainer extends Component {
   componentDidMount() {
     const breed = this.props.match.params.breed;
     fetch(`https://dog.ceo/api/breed/${encodeURIComponent(breed)}/images`)
-    .then(res => res.json)
-    .then(data => this.updateImages(data.message))
-    .catch(console.error);
+      .then(res => res.json())
+      .then(data => this.updateImages(data.message))
+      .catch(console.error);
   }
 
   //encodeURIComponent: When creating URIs in code using string templates or string concatenation, we have to make sure that the result is a valid URI. If you use a variable (like breed in the above example), it may refer to a string containing a / or a ?. Those symbols have special meaning in URIs, so they need to be escaped/encoded. That's what encodeURIComponent is for.
