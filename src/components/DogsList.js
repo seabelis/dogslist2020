4edf6a8d0bfbe11edb1da1a8ce09a7d54bc26edf
seabelis <seabelis74@gmail.com>
2020-11-06 13:38:56 +0100

    add class to li

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 export default class DogsList extends Component {
   renderDogBreed(breed) {
-    return <li key={breed}> 
+    return <li className="dog-breed" key={breed}> 
       <Link to={ `/dog-breeds/${breed}` }>{breed}</Link>
       </li>
   }
